97ef4f6af3618a79d8704c7b0604ef697f037269
Laucardona <lauris.042006@gmail.com>
2026-02-23 23:51:22 -0500

    feat(Api-fetch)

--- javascript/dom.js
+++ javascript/dom.js
@@ -2,26 +2,32 @@ function getUserCard(user) {
     return `<article class="user-card">
     <div class="user-header">
         <h2>${user.name}</h2>
-        <span>${user.status} - ${user.species}</span>
+        <span>ID: ${user.id} • ${user.status} - ${user.species}</span>
     </div>
 
     <div class="user-content">
+
         <div class="info-block">
             <strong>Información</strong>
-            Género: ${user.gender}
-            <br />
-            Origen: ${user.origin.name}
+            Género: ${user.gender} <br />
+            Origen: ${user.origin.name} <br />
+            Ubicación actual: ${user.location.name}
         </div>
 
         <div class="info-block">
+            <strong>Detalles</strong>
+            Episodios: ${user.episode.length} <br />
+            Creado en API: ${new Date(user.created).toLocaleDateString()}
+        </div>
+
         <div class="info-block company">
             <strong>Imagen</strong>
-            <img src="${user.image}" alt="${user.name}" width="100%">
+            <img src="${user.image}" alt="${user.name}">
         </div>
+
     </div>
 </article>`;
 }
-
 export function printUsers(users) {
     users.forEach((user) => {
         const card = getUserCard(user);
